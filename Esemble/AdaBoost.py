b39a2e3e7aee35b771125908e902457f7ea018c8
Hyunjun-Bruce-Lee <hyunjun.bruce.lee@gmail.com>
2021-12-07 15:01:39 +0900

    ADD Ada Boost

---
+++ Esemble/AdaBoost.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import classification_report
+
+# iris 데이터를 읽어온다.
+iris = load_iris()
+
+# Train 데이터 세트와 Test 데이터 세트를 구성한다
+trainX, testX, trainY, testY = \
+    train_test_split(iris['data'], iris['target'], test_size = 0.2)
+
+svm = SVC(kernel='rbf', gamma=0.1, C=1.0, probability=True)
+aboost = AdaBoostClassifier(base_estimator=svm, n_estimators=100)
+aboost.fit(trainX, trainY)
+
+# 학습데이터와 시험데이터의 정확도를 측정한다.
+print('\n학습데이터의 정확도 = %.2f' % aboost.score(trainX, trainY))
+print('시험데이터의 정확도 = %.2f' % aboost.score(testX, testY))
+
+# 시험데이터의 confusion matrix를 작성한다 (row : actual, col : predict)
+predY = aboost.predict(testX)
+print('\nConfusion matrix :')
+print(confusion_matrix(testY, predY))
+print()
+print(classification_report(testY, predY, target_names=iris.target_names))
+
+
